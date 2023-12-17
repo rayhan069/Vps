94952dd59c14b4f42daa2844cb77dd5c5ae3e3e9
rayhan069 <152002505+rayhan069@users.noreply.github.com>
2023-12-17 14:51:54 +0600

    ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -3,4 +3,4 @@ wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok config add-authtoken 2ZeoVsnJtJtur0u07iVf0ApjVLq_umPrynce4VVEXST3yCPk #Put Yours here
 export TERM=xterm
-./ngrok http 4421
+./ngrok http 25565
